8e1ddfb7bdad0cc28c55c4cbd407c4e9a0437ec7
Jerther <jerther@gmail.com>
2018-03-04 15:05:48 -0500

    [WIP] Amelioration de la form view des inspections.

--- inspection911/models/inspection.py
+++ inspection911/models/inspection.py
@@ -3,9 +3,11 @@ from odoo import fields, models
 
 class Inspection(models.Model):
     _name = 'inspection'
+    _rec_name = 'device_id'
+
     fire_station_id = fields.Many2one('fire_station', "Fire Station", required=True, ondelete='cascade')
     device_id = fields.Many2one('device', "Device", required=True, ondelete='cascade')
-    user_id = fields.Many2one('res.users', "User", required=True, ondelete='cascade')
+    user_id = fields.Many2one('res.users', "User", required=True, ondelete='cascade', default=lambda self: self.env.user.id)
     complies = fields.Boolean()
     comment = fields.Html()
     date = fields.Datetime(required=True, default=fields.Datetime.now())

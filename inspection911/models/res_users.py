a8b2db6bb23c58aefb7ed8d2d0807a386639d196
Jerther <jerther@gmail.com>
2018-05-26 15:07:43 -0400

    [FIX] rename res_users.number to fireman_number and add to form view

--- inspection911/models/res_users.py
+++ inspection911/models/res_users.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class ResUsers(models.Model):
     _inherit = 'res.users'
     fire_station_ids = fields.Many2many('inspection911.fire_station', 'fire_station_users_rel', string='Fire Stations')
-    number = fields.Char(help="License number")
+    fireman_number = fields.Char(help="License number")
